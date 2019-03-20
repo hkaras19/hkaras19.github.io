36c1c9304fc621c7c3a216fd51349fcd8a695031
Hunter Karas <hunterkaras808@gmail.com>
2019-03-19 19:37:26 -1000

    Login
    Creating Login page

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
 // Variables
 var purchaseType = "";
+var supportEmail = "";
+var accessCode = "";
+var organizationName = "";
 
+// Transistions from the purchase type form to the specific form
 function showNextForm()
 {
     var radios = document.getElementsByName('formButton');
@@ -10,26 +14,25 @@ function showNextForm()
     {
         if (radios[i].checked)
         {
-
             switch (radios[i].value)
             {
                 case "Private Organization":
                     purchaseType = "Private Organization"
                     document.getElementById("purchaseTypeForm").style.display = "none"
                     document.getElementById("privateOrganizationForm").style.display = "inline"
-                    break;
+                    return;
 
                 case "Community":
                     purchaseType = "Community"
                     document.getElementById("purchaseTypeForm").style.display = "none"
                     document.getElementById("communityForm").style.display = "inline"
-                    break;
+                    return;
 
                 case "Individual Chat":
                     purchaseType = "Individual Chat"
                     document.getElementById("purchaseTypeForm").style.display = "none"
                     document.getElementById("individualChatForm").style.display = "inline"
-                    break;
+                    return;
 
                 default:
                     break;
@@ -49,7 +52,73 @@ function showFirstForm()
     document.getElementById("individualChatForm").style.display = "none"
 }
 
+
 function showPaymentForm()
 {
-    alert(purchaseType);
+    document.getElementById("purchaseTypeForm").style.display = "none"
+    document.getElementById("privateOrganizationForm").style.display = "none"
+    document.getElementById("communityForm").style.display = "none"
+    document.getElementById("individualChatForm").style.display = "none"
 }
+
+function showChatCreate()
+{
+    if (purchaseType == "Private Organization")
+    {
+        supportEmail = document.getElementById("Private support email").value;
+        accessCode = document.getElementById("Private password").value;
+        organizationName = document.getElementById("Private organization name").value;
+    }
+    else
+    {
+        supportEmail = document.getElementById("Support email").value;
+        organizationName = document.getElementById("Organization name").value;
+    }
+
+    document.getElementById("privateOrganizationForm").style.display = "none"
+    document.getElementById("communityForm").style.display = "none"
+}
+
+
+function login()
+{
+    alert("Here")
+    let email = document.getElementById("userLoginEmail").value;
+    let password = document.getElementById("userLoginPassword").value;
+
+    if (email == "" || password == "")
+    {
+        alert("Please fill out the information!");
+    }
+    else
+    {
+        alert("Here")
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) 
+        {
+            var errorMessage = error.message;
+            alert("Error: " + errorMessage);
+        });
+    }      
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) 
+    {
+        alert("Logged In")
+      // User is signed in.
+    //   var displayName = user.displayName;
+    //   var email = user.email;
+    //   var emailVerified = user.emailVerified;
+    //   var photoURL = user.photoURL;
+    //   var isAnonymous = user.isAnonymous;
+    //   var uid = user.uid;
+    //   var providerData = user.providerData;
+      // ...
+    } 
+    else 
+    {
+        alert("No User")
+      // User is signed out.
+      // ...
+    }
+  });
